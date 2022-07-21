5a8c3f4d9f88a0ef256fe50c3d161afcbf71b9ae
Grigory3v <jack977j@gmail.com>
2022-07-21 20:43:04 +0400

    Добавил task_02

--- task_02/Program.cs
+++ task_02/Program.cs
@@ -1,9 +1,16 @@
-﻿/*
-Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+﻿
+/*
 
-[3, 7, 23, 12] -> 19
+Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+и возвращает значение этого элемента или же указание, что такого элемента нет.
+Например, задан массив:
+1 4 7 2
 
-[-4, -6, 89, 6] -> 0
+5 9 2 3
+
+8 4 2 4
+
+17 -> такого числа в массиве нет
 */
 
 namespace Task_02
@@ -12,34 +19,35 @@ namespace Task_02
     {
         public static void Main()
         {
-            Console.WriteLine($" Введите длину массива");
-            int size = Convert.ToInt32(Console.ReadLine());
-            int [] array = new int[size];
-            Console.Write($"одномерный массив, заполненный случайными числами = [ ");
-            for ( int i = 0; i < array.Length; i++ )
-            {   
-                array[i] = new Random().Next(-100, 100);
-                Console.Write($"{array[i]} ");
-            }
-            Console.Write($"]");
-            Console.WriteLine();
-            PrintArray(array);   
-
-        }
-        public static void PrintArray (int[] array)
-        {
-            int length = array.Length;
-            int count = 0;
-            for (int i = 0; i < length; i++ )
             {
-                if ( i % 2 == 0 )
-                {
-                    count = count + array[i];    
-                }
-            }
-            Console.Write($" суммa элементов, стоящих на нечётных позициях = {count} ");
-            Console.WriteLine();
-        }
+                Console.WriteLine($" Введите позицию элемента в двумерном массиве:");
+                int positionArray = Convert.ToInt32(Console.ReadLine());
 
+                double[,] array = new double[,]
+                {
+                {1, 4 ,7 ,2},
+                {5 ,9 ,2 ,3},
+                {8 ,4 ,2 ,4}
+                };
+                if (positionArray % 10 <= array.GetLength(0) && positionArray / 10 <= array.GetLength(1))
+                {
+                    for (int i = 0; i < array.GetLength(0); i++)
+                    {
+                        for (int j = 0; j < array.GetLength(1); j++)
+                        {
+                            if (positionArray % 10 == i && positionArray / 10 == j)
+                            {
+                                Console.Write($"значение этого элемента двумерном массиве =  ");
+                                Console.WriteLine($"{array[i, j]} ");
+                            }
+                        }
+
+                    }
+                }
+                else Console.Write($"такого числа в массиве нет");
+
+            }
+
+        }
     }
 }
