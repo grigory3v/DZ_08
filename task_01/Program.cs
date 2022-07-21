b82782a6bcda1cd8699874654ac84f40fc480e81
Grigory3v <jack977j@gmail.com>
2022-07-21 20:42:55 +0400

    Добавил task_01

--- task_01/Program.cs
+++ task_01/Program.cs
@@ -1,7 +1,13 @@
 ﻿/* 
-Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
 
-[345, 897, 568, 234] -> 2
+m = 3, n = 4.
+
+0,5 7 -2 -0,2
+
+1 -3,3 8 -9,9
+
+8 7,8 -7,1 9
 */
 
 namespace Task_01
@@ -10,33 +16,38 @@ namespace Task_01
     {
         public static void Main()
         {
-            Console.WriteLine($" Введите длину массива");
-            int size = Convert.ToInt32(Console.ReadLine());
-            int [] array = new int[size];
-            Console.Write($"массив заполненный случайными положительными трёхзначными числами = [ ");
-            for ( int i = 0; i < array.Length; i++ )
-            {   
-                array[i] = new Random().Next(100, 1000);
-                Console.Write($"{array[i]} ");
-            }
-            Console.Write($"]");
-            Console.WriteLine();
-            PrintArray(array);   
+            int m = 3; //количество строк массива
+            int n = 4; //количество столбцов массива
 
-        }
-        public static void PrintArray (int[] array)
-        {
-            int length = array.Length;
-            int count = 0;
-            for (int i = 0; i < length; i++ )
-            {
-                if ( array[i] % 2 == 0 )
-                {
-                    count++;    
-                }
-            }
-            Console.Write($" количество чётных чисел в массиве = {count} ");
+            double[,] array = new double[m, n];
+            Console.Write($"двумерный массив размером m×n, заполненный случайными вещественными числами =  ");
             Console.WriteLine();
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    array[i, j] = new Random().Next(-100, 100);
+                    Console.Write($"{array[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            //     PrintArray(array);   
+
+            // }
+            // public static void PrintArray (int[] array)
+            // {
+            //     int length = array.Length;
+            //     int count = 0;
+            //     for (int i = 0; i < length; i++ )
+            //     {
+            //         if ( array[i] % 2 == 0 )
+            //         {
+            //             count++;    
+            //         }
+            //     }
+            //     Console.Write($" количество чётных чисел в массиве = {count} ");
+            //     Console.WriteLine();
         }
 
     }
