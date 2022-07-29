501f5af42e11ca97f4492b7912d5274081b4ac2a
Grigory3v <jack977j@gmail.com>
2022-07-29 18:29:06 +0400

    Добавил task_02

--- task_02/Program.cs
+++ task_02/Program.cs
@@ -1,16 +1,19 @@
 ﻿
 /*
 
-Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
-и возвращает значение этого элемента или же указание, что такого элемента нет.
+Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
 Например, задан массив:
+
 1 4 7 2
 
 5 9 2 3
 
 8 4 2 4
 
-17 -> такого числа в массиве нет
+5 2 6 7
+
+Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
 */
 
 namespace Task_02
@@ -19,35 +22,34 @@ namespace Task_02
     {
         public static void Main()
         {
+
+            double[,] array = new double[,]
             {
-                Console.WriteLine($" Введите позицию элемента в двумерном массиве:");
-                int positionArray = Convert.ToInt32(Console.ReadLine());
-
-                double[,] array = new double[,]
+                {1, 4, 7, 2},
+                {5, 9, 2, 3},
+                {8, 4, 2, 4},
+                {5, 2, 6, 7}
+            };
+            int minSumStr = 0;
+            double min = 0;
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                double sum = 0;
+                for (int j = 0; j < array.GetLength(1); j++)
                 {
-                {1, 4 ,7 ,2},
-                {5 ,9 ,2 ,3},
-                {8 ,4 ,2 ,4}
-                };
-                if (positionArray % 10 <= array.GetLength(0) && positionArray / 10 <= array.GetLength(1))
-                {
-                    for (int i = 0; i < array.GetLength(0); i++)
-                    {
-                        for (int j = 0; j < array.GetLength(1); j++)
-                        {
-                            if (positionArray % 10 == i && positionArray / 10 == j)
-                            {
-                                Console.Write($"значение этого элемента двумерном массиве =  ");
-                                Console.WriteLine($"{array[i, j]} ");
-                            }
-                        }
-
-                    }
+                    sum += array[i, j];
                 }
-                else Console.Write($"такого числа в массиве нет");
-
+                if (i == 0)
+                {
+                    min = sum;
+                }
+                if (sum <= min)
+                {
+                    min = sum;
+                    minSumStr = i + 1;
+                }
             }
-
+            Console.WriteLine($" номер строки с наименьшей суммой элементов: {minSumStr} строка  ");
         }
     }
 }
