1b2d0bad9d35ff4f83f8c765d0ccdcc4f9a18fe7
Grigory3v <jack977j@gmail.com>
2022-07-29 18:28:59 +0400

    Добавил task_01

--- task_01/Program.cs
+++ task_01/Program.cs
@@ -1,13 +1,21 @@
 ﻿/* 
-Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по возрастанию элементы каждой строки двумерного массива.
 
-m = 3, n = 4.
+Например, задан массив:
 
-0,5 7 -2 -0,2
+1 4 7 2
 
-1 -3,3 8 -9,9
+5 9 2 3
 
-8 7,8 -7,1 9
+8 4 2 4
+
+В итоге получается вот такой массив:
+
+1 2 4 7
+
+2 3 5 9
+
+2 4 4 8
 */
 
 namespace Task_01
@@ -16,39 +24,31 @@ namespace Task_01
     {
         public static void Main()
         {
-            int m = 3; //количество строк массива
-            int n = 4; //количество столбцов массива
 
-            double[,] array = new double[m, n];
-            Console.Write($"двумерный массив размером m×n, заполненный случайными вещественными числами =  ");
-            Console.WriteLine();
-            for (int i = 0; i < m; i++)
+            double[,] array = new double[,]
             {
-                for (int j = 0; j < n; j++)
+                {1, 4, 7, 2},
+                {5, 9, 2, 3},
+                {8, 4, 2, 4}
+            };
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                double number = 0;
+                for (int j = 0; j < array.GetLength(1); j++)
                 {
-                    array[i, j] = new Random().Next(-100, 100);
-                    Console.Write($"{array[i, j]} ");
+                    for (int k = array.GetLength(1) - 1; k != j; k--)
+                    {
+                        if (array[i, j] > array[i, k])
+                        {
+                            number = array[i, k];
+                            array[i, k] = array[i, j];
+                            array[i, j] = number;
+                        }
+                    }
+                    Console.Write($" {array[i, j]} ");
                 }
                 Console.WriteLine();
             }
-            Console.WriteLine();
-            //     PrintArray(array);   
-
-            // }
-            // public static void PrintArray (int[] array)
-            // {
-            //     int length = array.Length;
-            //     int count = 0;
-            //     for (int i = 0; i < length; i++ )
-            //     {
-            //         if ( array[i] % 2 == 0 )
-            //         {
-            //             count++;    
-            //         }
-            //     }
-            //     Console.Write($" количество чётных чисел в массиве = {count} ");
-            //     Console.WriteLine();
         }
-
     }
 }
